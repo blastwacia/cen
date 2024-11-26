6411d066263ded25817778a89e42fe46c0f2ef5b
blastwacia <ciatotoappdana@gmail.com>
2024-11-26 17:05:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -129,4 +129,5 @@ def start_blasting():
         return jsonify({"status": "error", "message": f"An error occurred: {str(e)}"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
+
